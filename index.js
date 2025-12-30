00412ea63789f671c585a71c99ddb38866370d25
raushan3405 <raushan3405@gmail.com>
2025-12-30 16:58:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 // Coolify / cloud platforms ke liye PORT env use karna best practice hai
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Frmply World");
-});
+// Static files serve karne ke liye middleware
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
