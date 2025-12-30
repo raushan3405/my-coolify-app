b1dbf1175c492e65711abe4cc5186ba508e3087f
raushan3405 <raushan3405@gmail.com>
2025-12-30 15:49:51 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// Coolify / cloud platforms ke liye PORT env use karna best practice hai
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Coolify + GitHub + Node.js!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
