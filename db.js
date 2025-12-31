4e1dd70d2c5291719aaeb9398baf57b86a57ce50
Raushan Kumar <raushan3405@gmail.com>
2025-12-31 23:33:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 // Create a new connection pool using environment variables
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: false,
 });
 
 // Export a query function that uses the pool
